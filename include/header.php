18a9095b70441394f8fb29a0714c8b6f8b14ef33
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2016-10-04 14:49:47 +0200

    drugi kommit
    hellloo

---
+++ include/header.php
@@ -0,0 +1,27 @@
+<div id="header">
+    
+    <div class="slika">
+        
+    </div>
+    
+    <div class="svasta">
+        <div class="sivo">
+            <h1>Samuel anderson</h1>
+        </div>
+        <div class="sivo">
+            <h2>js generated text</h2>
+        </div>
+        <nav>
+            <ul id="nav">
+                <li id="home"> <a href="">home</a></li>
+                <li id="resume"><a href="">resume</a></li>
+                <li id="portfolio"><a href="">portfolio</a></li>
+                <li id="contacts"><a href="">contacts</a></li>
+                <li id="feedback"><a href="">feedback</a></li>
+                <li id="blog"><a href="">blog</a></li>
+            </ul>
+        </nav>
+        
+    </div>
+    
+</div>
